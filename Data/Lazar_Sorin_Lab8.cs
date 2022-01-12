619b7f79528119fc062374b599b0a266990c219b
LazarSorin <sorinel_lazar2008@yahoo.com>
2022-01-12 14:39:40 +0200

    Add project files.

---
+++ Data/Lazar_Sorin_Lab8.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Lazar_Sorin_Lab8.Models;
+
+namespace Lazar_Sorin_Lab8.Data
+{
+    public class Lazar_Sorin_Lab8Context : DbContext
+    {
+        public Lazar_Sorin_Lab8Context (DbContextOptions<Lazar_Sorin_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lazar_Sorin_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Lazar_Sorin_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Lazar_Sorin_Lab8.Models.Category> Category { get; set; }
+    }
+}
